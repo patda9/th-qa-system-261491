b90cbe3e3113d1c2cd9f076da6d4ab1764aceede
Patdanai <patdanai.duangwaropas@gmail.com>
2019-01-17 17:08:46 +0700

    preprocess samples, split to test/train form training model

--- src/sentence-vectorization.py
+++ src/sentence-vectorization.py
@@ -8,7 +8,7 @@ np.random.seed(0)
 
 path = 'C:/Users/Patdanai/Desktop/wiki-dictionary-[1-50000]/'
 dataset = os.listdir(path)
-print(dataset.__len__())
+# print(dataset.__len__())
 
 n = 100
 sample_ids = []
@@ -18,7 +18,7 @@ for i in range(n):
         randomed_doc_id = int(dataset[np.random.randint(dataset.__len__())].split('.')[0])
     sample_ids.append(randomed_doc_id)
 
-print(sample_ids, sample_ids.__len__())
+# print(sample_ids, sample_ids.__len__())
 
 count = 1
 samples = []
@@ -34,11 +34,11 @@ for article_id in sample_ids:
 
 for i in range(samples.__len__()):
     samples[i] = prep.remove_xml(samples[i])
-print(samples[-1])
+# print(samples[-1])
 vocabularies = [w for doc in samples for w in doc]
-print(vocabularies[-1])
+# print(vocabularies[-1])
 vocabularies = prep.remove_stop_words(vocabularies)
-print(vocabularies[-1])
+# print(vocabularies[-1])
 
 for i in range(samples.__len__()):
     samples[i] = prep.remove_xml(samples[i])
@@ -46,39 +46,40 @@ for i in range(samples.__len__()):
 
 ## word to id transformation
 word2id = {}
-word2id['<NIV>'] = 0
-for (i, w) in enumerate(set(vocabularies), 1):
+word2id['<PAD>'] = 0
+word2id['<NIV>'] = 1
+for (i, w) in enumerate(set(vocabularies), 2):
     try:
         word2id[w] = i
     except ValueError:
-        word2id[w] = 0
+        word2id[w] = 1
 
 id2word = {idx: w for w, idx in word2id.items()}
 
-print(id2word)
+# print(id2word)
 
-sample2id = {i: article_id for i, article_id in enumerate(list(sample_ids))}
-print(sample2id)
+sample2id = {article_id: i for i, article_id in enumerate(list(sample_ids))}
+# print(sample2id)
 
 ## words to word ids representation
 id_representation = []
 for i in range(samples.__len__()):
     id_representation.append([word2id[w] for w in samples[i]])
 
-print(id_representation)
+# print(id_representation)
 
-doc_ids_remapping = []
+remapped_article_ids = []
 for i in range(sample_ids.__len__()):
-    doc_ids_remapping.append(sample2id[i])
+    remapped_article_ids.append(sample2id[sample_ids[i]])
 
-print(doc_ids_remapping)
+# print(remapped_article_ids)
 
-words_per_sentence = 10
+words_per_sentence = 20
 article_sentences = id_representation.copy()
 for i in range(article_sentences.__len__()):
-    article_sentences[i] = [article_sentences[i][j*words_per_sentence : (j+1)*words_per_sentence] for j in range((article_sentences[i].__len__() + n-1) // n)]
+    article_sentences[i] = [article_sentences[i][j*words_per_sentence : (j+1)*words_per_sentence] for j in range((article_sentences[i].__len__() + words_per_sentence-1) // words_per_sentence)]
 
-print('*', article_sentences[0])
+# print('*', article_sentences[0])
 
 # https://www.facebook.com/LADbible/videos/375311296563614/
 
@@ -86,11 +87,30 @@ from keras.preprocessing import sequence
 
 padded_article_sentences = []
 for i in range(article_sentences.__len__()):
-    try:
-        padded_s = sequence.pad_sequences(article_sentences[i], maxlen=words_per_sentence)
-        padded_article_sentences.append()
-    except ValueError:
-        pass
+    padded_s = sequence.pad_sequences(article_sentences[i], maxlen=words_per_sentence)
+    padded_article_sentences.append(padded_s)
+
+# print(padded_article_sentences)
+
+sentences_article_ids = []
+for i in range(padded_article_sentences.__len__()):
+    for j in range(padded_article_sentences[i].__len__()):
+        sentences_article_ids.append(remapped_article_ids[i])
+# print(sentences_article_ids)
+
+flatten_article_sentences = np.vstack(padded_article_sentences)
+# print(list(zip(flatten_article_sentences, sentences_article_ids)))
+
+from keras.utils import to_categorical
+
+x = flatten_article_sentences.copy()
+y = to_categorical(sentences_article_ids, num_classes=sample2id.__len__())
+
+x_train = x[:int(.8 * x.__len__())]
+y_train = y[:int(.8 * y.__len__())]
+
+x_test = x[int(.8 * x.__len__()):]
+y_test = y[int(.8 * y.__len__()):]
 
 from keras.layers import Activation, Bidirectional, Dense, Embedding, Flatten, InputLayer, LSTM, TimeDistributed
 from keras.layers.core import Masking
@@ -99,6 +119,14 @@ from keras.optimizers import Adam
 from keras.utils import to_categorical
 
 embedding_size = 64
+lstm_output_size = 128
 
 model = Sequential()
-# model.add(Embedding(word2id.__len(), ))
+model.add(Embedding(word2id.__len__(), embedding_size, input_length=words_per_sentence))
+model.add(Masking(mask_value=0, input_shape=(words_per_sentence, 1)))
+model.add(Bidirectional(LSTM(lstm_output_size)))
+model.add(Dense(64, activation='tanh'))
+model.add(Dense(x_train.__len__(), activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+model.summary()
