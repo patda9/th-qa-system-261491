c43b49c7f0d1ed95573a9b83c45095f5e33c72b3
Patdanai <patdanai.duangwaropas@gmail.com>
2019-01-30 20:05:33 +0700

    add original character positions tracking

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,9 +3,10 @@ import re
 
 # defining regex pattern
 # pattern = re.compile(r"[^\u0E00-\u0E7F0-9]|^'|'$|''")
+path = 'C:/Users/Patdanai/Desktop/wiki-dictionary-[1-50000]/' # default development path
 
 def load_article(dir_path, art_id):
-    with open(dir_path + art_id + '.json', 'r', encoding='utf-8', errors='ignore') as f:
+    with open(dir_path + str(art_id) + '.json', 'r', encoding='utf-8', errors='ignore') as f:
         article = json.load(f)
     return article
 
@@ -17,11 +18,56 @@ def remove_xml(article):
             xml_close_index = i + 1
     return article[xml_close_index:]
 
-# remove english, special char, *(and stop words)  
-def remove_stop_words(article, pattern=re.compile(r"[^\u0E00-\u0E7F^0-9]||[^0-9.]|^'|'$|''")):
-    for i in range(len(article)):
+# remove english, special char, *(and stop words) returns remaining words from preprocessing and origin cumulative string lengths
+def remove_noise(article, pattern=re.compile(r"[^\u0E00-\u0E7F^0-9^ \t^.^,]")):
+    list_with_char_removed = []
+    word_lengths = []
+    for i in range(article.__len__()):
         char_to_remove = re.findall(pattern, article[i])
-        list_with_char_removed = [char for char in article[i] if(not(char in char_to_remove))]
-        article[i] = ''.join(list_with_char_removed)
-    return list(filter(None, article))
-    
+        temp = ''
+        word_lengths.append(article[i].__len__())
+        for j in range(article[i].__len__()):
+            if(not(article[i][j] in char_to_remove)):
+                temp += article[i][j]
+        if(temp.__len__() and not(temp in re.findall(r"^\s|^,|^.", temp))):
+            list_with_char_removed.append((i, temp))
+    word_locations = word_lengths.copy()
+    summation = 0
+    for i in range(word_locations.__len__()):
+        summation += word_locations[i]
+        word_locations[i] = summation
+    # print(word_lengths[151])
+    # print(word_locations[150] + 1, word_locations[151] + 1)
+    return list_with_char_removed, word_locations
+
+# main function for script testing
+if(__name__ == '__main__'):
+    test_sample = load_article(path, 115035)    
+    test_sample = remove_noise(test_sample)
+    # print(remove_noise(test_sample)[0])
+    # print(remove_noise(test_sample)[1])
+
+    answer_details = {}
+    with open('./../new_sample_questions.json', 'r', encoding='utf-8', errors='ignore') as f:
+        answer_details = json.load(f)
+
+    answer_char_locs = []
+    for q in answer_details['data']:
+        answer_begin_pos = q['answer_begin_position '] - 1 # array index => (pos - 1)
+        answer_end_pos = q['answer_end_position'] - 1 # array index => (pos - 1)
+        answer_char_locs.append((answer_begin_pos, answer_end_pos))
+
+    # # prototype: convert to original character location
+    # ## i loop gets index
+    # for i in range(test_sample[1].__len__()):
+    #     b = answer_char_locs[0][0]
+    #     e = answer_char_locs[0][1] ## ending position of word
+    #     if(test_sample[1][i] == e):
+    #         word_number = i
+    #         print('preprocessed word number:', word_number)
+    #         print('begin character location:', b, '| ending character location:', e)
+    #         print('origin begin character location:', b + 1, '| original ending character location:', e + 1)
+    #         ## j loop gets answer
+    #         for j in range(test_sample[0].__len__()):
+    #             if(word_number == test_sample[0][j][0]):
+    #                 print('word:', test_sample[0][j][1])
--- src/similarity_mesurement.py
+++ src/similarity_mesurement.py
@@ -2,6 +2,7 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import preprocessing as prep
+import re
 import sentence_vectorization as sv
 import os
 
@@ -41,7 +42,7 @@ if(__name__ == '__main__'):
             selected_questions_plain_text.append(q['question'])
             count += 1
     
-    # print(list(zip(selected_questions_ids, selected_article_ids)))
+    print(list(zip(selected_questions_ids, selected_article_ids)))
 
     # load each article content
     selected_articles = [] # plain text
@@ -53,10 +54,7 @@ if(__name__ == '__main__'):
         selected_articles.append(''.join(article))
         selected_tokenized_articles.append(article)
 
-    # print(selected_articles)
-    # print(selected_tokenized_articles)
-
-    # remove whitespaces from questions
+    # remove noise from questions
     selected_questions = []
     for i in range(questions.__len__()):
         if((i+1) in selected_questions_ids): # i+1: first question starts at 1 
@@ -71,54 +69,69 @@ if(__name__ == '__main__'):
         file_path = os.path.join(dataset_path, str(i) + '.json')
         with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
             data = json.load(f)
-            data = prep.remove_xml(data)
-            data = prep.remove_stop_words(data)
+            data = prep.remove_noise(data)
         preprocessed_articles.append(data)
         j += 1
     
-    # print(preprocessed_articles[1])
+    # preprocessed_articles[i][0] => (index of /*(starts from 0)*/, word)
+    # preprocessed_articles[i][1] => list of cumulative string length (end position of words)
 
-    vocabularies = [word for article in list(preprocessed_articles) for word in article]
+    remaining_words = []
+    cumulative_word_lengths = []
+    for i in range(preprocessed_articles.__len__()):
+        remaining_words.append(preprocessed_articles[i][0])
+        cumulative_word_lengths.append(preprocessed_articles[i][1])
+
+    print(remaining_words[0])
+    print(cumulative_word_lengths[1])
+
+    vocabularies = [article[i][1] for article in remaining_words for i in range(article.__len__())]
     
     word2id = {}
     word2id['<PAD>'] = 0
-    word2id['<NIV>'] = 1
-    for (i, w) in enumerate(set(vocabularies), 2):
+    for (i, w) in enumerate(set(vocabularies), 1):
         try:
             word2id[w] = i
         except ValueError:
-            print('niv')
-            word2id[w] = 1
+            pass
 
     id2word = {idx: w for w, idx in word2id.items()}
     
     # print(word2id)
 
+    pattern = re.compile(r"[^\u0E00-\u0E7F^0-9^ \t^.^,]")
     selected_question_id_representation = []
     for i in range(selected_questions.__len__()):
-        selected_questions[i] = prep.remove_stop_words(selected_questions[i])
+        temp_list = []
+        for j in range(selected_questions[i].__len__()):
+            char_to_remove = re.findall(pattern, selected_questions[i][j])
+            temp_word = ''
+            for k in range(selected_questions[i][j].__len__()):
+                if(not(selected_questions[i][j] in char_to_remove)):
+                    temp_word += selected_questions[i][j][k]
+            temp_list.append(temp_word)
+        selected_questions[i] = temp_list
+
     for q in selected_questions:
         temp = []
         for w in q:
             try:
                 temp.append(word2id[w])
             except KeyError:
-                temp.append(word2id['<NIV>'])
-                print(w)
+                pass
         selected_question_id_representation.append(temp)
 
-    # exit()
     # print(selected_question_id_representation)
     # print(selected_articles[-1])
 
     content_id_representation = []
-    for q in preprocessed_articles:
+    for q in remaining_words:
         temp = []
         for w in q:
             try:
-                temp.append(word2id[w])
+                temp.append(word2id[w[1]])
             except KeyError:
-                temp.append(word2id['<NIV>'])
+                pass
         content_id_representation.append(temp)
 
     words_per_sentence = 20
@@ -126,6 +139,7 @@ if(__name__ == '__main__'):
     overlapping_words = words_per_sentence // 2
 
     preprocessed_article_id_representation = sv.k_words_separate(words_per_sentence, content_id_representation, overlap=overlap_flag)
+    # exit()
 
     from keras.preprocessing import sequence
     
@@ -171,39 +185,38 @@ if(__name__ == '__main__'):
         answer = selected_articles[i][answer_char_locs[i][0]:answer_char_locs[i][-1]]
         answers.append(answer)
 
+    # # find ans index
+    # answer_indexes = []
+    # for i in range(selected_tokenized_articles.__len__()):
+    #     current_char_loc = 0
+    #     for j in range(selected_tokenized_articles[i].__len__()):
+    #         for c in selected_tokenized_articles[i][j]:
+    #             if(current_char_loc == answer_char_locs[i][0]):
+    #                 answer_indexes.append([j, answers[i]])
+    #             current_char_loc += 1
+
+    # # print(answer_indexes)
+
+    # answers_after_removed_stop_words = []
+    # for i in range(preprocessed_articles.__len__()):
+    #     answer_index_after_removed_stop_words = 0
+    #     all_answer_indexes = []
+    #     for j in range(preprocessed_articles[i].__len__()):
+    #         if(remaining_words[i][j] == remaining_words[i][1]):
+    #             all_answer_indexes.append(j)
+    #     answer_index_after_removed_stop_words = 0
+    #     for k in all_answer_indexes:
+    #         if(np.abs(answer_indexes[i][0] - k) < np.abs(answer_indexes[i][0] - answer_index_after_removed_stop_words)):
+    #             answer_index_after_removed_stop_words = k
+    #     answers_after_removed_stop_words.append(answer_index_after_removed_stop_words)
     
-    # find ans index
-    answer_indexes = []
-    for i in range(selected_tokenized_articles.__len__()):
-        current_char_loc = 0
-        for j in range(selected_tokenized_articles[i].__len__()):
-            for c in selected_tokenized_articles[i][j]:
-                if(current_char_loc == answer_char_locs[i][0]):
-                    answer_indexes.append([j, answers[i]])
-                current_char_loc += 1
+    # # print(answers_after_removed_stop_words)
 
-    # print(answer_indexes)
+    # # print(preprocessed_article_id_representation[-1])
 
-    answers_after_removed_stop_words = []
-    for i in range(preprocessed_articles.__len__()):
-        answer_index_after_removed_stop_words = 0
-        all_answer_indexes = []
-        for j in range(preprocessed_articles[i].__len__()):
-            if(preprocessed_articles[i][j].strip() == preprocessed_articles[i][1].strip()):
-                all_answer_indexes.append(j)
-        answer_index_after_removed_stop_words = 0
-        for k in all_answer_indexes:
-            if(np.abs(answer_indexes[i][0] - k) < np.abs(answer_indexes[i][0] - answer_index_after_removed_stop_words)):
-                answer_index_after_removed_stop_words = k
-        answers_after_removed_stop_words.append(answer_index_after_removed_stop_words)
-    
-    # print(answers_after_removed_stop_words)
-
-    # print(preprocessed_article_id_representation[-1])
-
-    for i in range(padded_selected_question_id_representation.__len__()):
-        selected_question_id_representation[i] = [id2word[idx] for idx in selected_question_id_representation[i]]
-        print(selected_question_id_representation[i])
+    # for i in range(padded_selected_question_id_representation.__len__()):
+    #     selected_question_id_representation[i] = [id2word[idx] for idx in selected_question_id_representation[i]]
+    #     print(selected_question_id_representation[i])
 
     # preprocess ของวินเนอร์ไม่เปลี่ยนตำแหน่งตัวอักษร
     # หาคำที่เป็นคำตอบก่อนจะได้ ['รา'] -> [468:469], ['บัต '] -> [470:473] แล้วค่อย remove stop words
@@ -226,65 +239,65 @@ if(__name__ == '__main__'):
     
     # print(dense1_layer_answer_outputs)
 
-    distance_matrix = []
-    for i in range(padded_content_id_representation.__len__()):
-        temp = []
-        for j in range(padded_content_id_representation[i].__len__()):
-            distance = np.linalg.norm(padded_content_id_representation[i][j] - padded_selected_question_id_representation[i])
-            temp.append(distance)
-        distance_matrix.append(temp)
+    # distance_matrix = []
+    # for i in range(padded_content_id_representation.__len__()):
+    #     temp = []
+    #     for j in range(padded_content_id_representation[i].__len__()):
+    #         distance = np.linalg.norm(padded_content_id_representation[i][j] - padded_selected_question_id_representation[i])
+    #         temp.append(distance)
+    #     distance_matrix.append(temp)
 
-    min_distance_indexes = []
-    for i in range(distance_matrix.__len__()):
-        min_index = np.asarray(distance_matrix[i]).argsort()[:distance_matrix[i].__len__()]
-        min_distance_indexes.append(min_index)
+    # min_distance_indexes = []
+    # for i in range(distance_matrix.__len__()):
+    #     min_index = np.asarray(distance_matrix[i]).argsort()[:distance_matrix[i].__len__()]
+    #     min_distance_indexes.append(min_index)
 
-    min_distance_indexes = np.asarray(min_distance_indexes)
+    # min_distance_indexes = np.asarray(min_distance_indexes)
     
-    # print(dense1_layer_question_outputs.__len__())
-    # print(dense1_layer_answer_outputs[0].__len__())
+    # # print(dense1_layer_question_outputs.__len__())
+    # # print(dense1_layer_answer_outputs[0].__len__())
 
-    # print(min_distance_indexes)
-    # print(distance_matrix)
-    # print(np.asarray(answers_after_removed_stop_words) // words_per_sentence)
+    # # print(min_distance_indexes)
+    # # print(distance_matrix)
+    # # print(np.asarray(answers_after_removed_stop_words) // words_per_sentence)
 
-    answer_matrix = np.asarray(answers_after_removed_stop_words) // words_per_sentence
+    # answer_matrix = np.asarray(answers_after_removed_stop_words) // words_per_sentence
     
-    # print(answer_matrix)
+    # # print(answer_matrix)
 
-    ranks = []
-    proportion_ranks = []
-    for i in range(min_distance_indexes.__len__()):
-        # if(answer_matrix[i] != 0):
-        print(min_distance_indexes[i][answer_matrix[i]], min_distance_indexes[i].__len__())
-        ranks.append(min_distance_indexes[i][answer_matrix[i]])
-        proportion_ranks.append((min_distance_indexes[i][answer_matrix[i]]) / min_distance_indexes[i].__len__())
-        # else:
+    # ranks = []
+    # proportion_ranks = []
+    # for i in range(min_distance_indexes.__len__()):
+    #     # if(answer_matrix[i] != 0):
+    #     print(min_distance_indexes[i][answer_matrix[i]], min_distance_indexes[i].__len__())
+    #     ranks.append(min_distance_indexes[i][answer_matrix[i]])
+    #     proportion_ranks.append((min_distance_indexes[i][answer_matrix[i]]) / min_distance_indexes[i].__len__())
+    #     # else:
 
-    print(proportion_ranks)
+    # print(proportion_ranks)
 
-    print(np.asarray(ranks))
-    # print(np.asarray(proportion_ranks))
+    # print(np.asarray(ranks))
+    # # print(np.asarray(proportion_ranks))
     
-    fig1, axs1 = plt.subplots(1)
-    axs1.hist(ranks, bins='auto')
-    title = str(words_per_sentence) + '-Words Sentence Model (from: ' + str(n_samples) + ' Samples)'
-    fig1.suptitle('N-ranks similarity between question and sentences in article', fontsize=12, fontweight='bold')
-    axs1.set_title(title)
-    axs1.set_xlabel('Closest to sentence rank (n-th)')
-    axs1.set_ylabel('Occurrence')
-    plt.savefig('./tmp1-' + str(words_per_sentence) + '.png')
+    # fig1, axs1 = plt.subplots(1)
+    # axs1.hist(ranks, bins='auto')
+    # title = str(words_per_sentence) + '-Words Sentence Model (from: ' + str(n_samples) + ' Samples)'
+    # fig1.suptitle('N-ranks similarity between question and sentences in article', fontsize=12, fontweight='bold')
+    # axs1.set_title(title)
+    # axs1.set_xlabel('Closest to sentence rank (n-th)')
+    # axs1.set_ylabel('Occurrence')
+    # plt.savefig('./tmp1-' + str(words_per_sentence) + '.png')
 
-    fig2, axs2 = plt.subplots(1)
-    axs2.hist(proportion_ranks, bins='auto')
-    axs2.set_title(title)
-    fig2.suptitle('N-ranks similarity between question and sentences in article', fontsize=12, fontweight='bold')
-    axs2.set_xlabel('Closest to sentence rank (n-th) / Article length (sentences)')
-    axs2.set_ylabel('Occurrence')
-    plt.savefig('./tmp2-' + str(words_per_sentence) + '.png')
+    # fig2, axs2 = plt.subplots(1)
+    # axs2.hist(proportion_ranks, bins='auto')
+    # axs2.set_title(title)
+    # fig2.suptitle('N-ranks similarity between question and sentences in article', fontsize=12, fontweight='bold')
+    # axs2.set_xlabel('Closest to sentence rank (n-th) / Article length (sentences)')
+    # axs2.set_ylabel('Occurrence')
+    # plt.savefig('./tmp2-' + str(words_per_sentence) + '.png')
     
-    for i in range(512):
-        for j in range(preprocessed_article_id_representation[i].__len__()):
-            if(j == answer_matrix[i]):
-                print(str(i) + ':', selected_questions_plain_text[i], proportion_ranks[i])
-                print([id2word[idx] for idx in preprocessed_article_id_representation[i][j]], '\n')
+    # for i in range(512):
+    #     for j in range(preprocessed_article_id_representation[i].__len__()):
+    #         if(j == answer_matrix[i]):
+    #             print(str(i) + ':', selected_questions_plain_text[i], proportion_ranks[i])
+    #             print([id2word[idx] for idx in preprocessed_article_id_representation[i][j]], '\n')
